32c883b814988b01d341af5d9329b5a86bfcd44b
saileshkasaju <leosailesh@gmail.com>
2017-04-12 14:44:02 +0545

    add userTile directive that swaps usename color on click

--- app/script.js
+++ app/script.js
@@ -3,6 +3,7 @@ angular.module('app', []);
 angular.module('app').controller('mainCtrl', function($scope) {
     $scope.user1 = {
         name: "Luke Skywalker",
+        selected: false,
         address: {
             street: 'P O Box 123',
             city: 'Kathmandu',
@@ -146,4 +147,19 @@ angular.module('app').directive('myClick', function($parse) {
            });
        }
    };
+});
+
+angular.module('app').directive('userTile', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            user: '='
+        },
+        templateUrl: 'userTile.html',
+        controller: function($scope) {
+            $scope.select = function() {
+                $scope.user.selected = !$scope.user.selected;
+            }
+        }
+    }
 });
